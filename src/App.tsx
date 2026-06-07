68628cdaa15994dc2469003785120db6a95343de
yeshayac <ifacohen@gmail.com>
2026-06-07 17:03:25 -0500

    Remove basename from HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <HashRouter basename="/ifacohen">
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
